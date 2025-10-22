385a9f71b8175186041de29b9d0086cb82af9173
Kamal Rathore <mr.rathoreji52@gmail.com>
2025-10-22 15:46:10 +0530

    created first spring project

---
+++ src/main/java/com/firstProject/meven/FirstSpringProjectApplication.java
@@ -0,0 +1,14 @@
+package com.firstProject.meven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringProjectApplication.class, args);
+		System.out.println("first spring project run successful");
+	}
+
+}
